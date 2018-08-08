9019d2798409e4f3a016b719e136201e31c479ae
chrislucas <christoffer.luccas@gmail.com>
2018-08-08 17:30:06 -0300

    solucao de exercicios sobre recursao

--- java/CoinChange/src/solution/CoinChangeImpl.java
+++ java/CoinChange/src/solution/CoinChangeImpl.java
@@ -1,8 +1,43 @@
 package solution;
 
+
+/**
+ * variacao interessante:
+ * https://www.geeksforgeeks.org/find-minimum-number-of-coins-that-make-a-change/
+ * */
+
 public class CoinChangeImpl {
 
-    public static void main(String[] args) {
 
+    public static long topDownApproach(long [] set,  long goal, int ith) {
+        /**
+         * se o valor objetivo for 0 quer dizer que existe uma solucao
+         * (valor objetivo 0 = uma possivel solucao eh nao usar moeda alguma {})
+         **/
+        if (goal == 0) {
+            return 1;
+        }
+        /**
+         * Nao existe solucao. Usamos um valor dentro do conjunto que nao cabe na solucao ou
+         * */
+        else if (goal < 0) {
+            return 0;
+        }
+        /**
+         * Nao existe mais moedas para usar e ainda nao chegamos a uma solucao
+         * */
+        else if(ith < 0) {
+            return 0;
+        }
+        long a = topDownApproach(set, goal, ith-1);
+        long b = topDownApproach(set, goal - set[ith], ith);
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        long [][] sets = { {1,2,3}, {2, 5, 3, 6}};
+        long goals [] = {4, 10};
+        int idx = 0;
+        System.out.println(topDownApproach(sets[idx], goals[idx], sets[idx].length-1));;
     }
 }
--- java/DavisStaircase/src/solution/StairCase.java
+++ java/DavisStaircase/src/solution/StairCase.java
@@ -15,9 +15,7 @@ public class StairCase {
     private static final BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
     private static final PrintWriter writer = new PrintWriter(new OutputStreamWriter(System.out), true);
 
-
     private static final long MOD = (long) (10E9+7);
-
 
     private static int sm(int a, int b) {
         return (int) ((a % MOD + b % MOD) % MOD);
@@ -35,27 +33,50 @@ public class StairCase {
         return sm(sm(a, b), c);
     }
 
-    private static int anotherWay(int n) {
-        if (n == 1)
+    private static int stepPerms(int n, int table []) {
+        if (n == 0) {
             return 1;
-
-        int m = n+1;
-        int memo[][] = new int[m][m];
-
-        for (int i = 0; i<m ; i++) {
-            memo[i][0] = 1;
         }
-        for (int i = 1; i<m ; i++) {
-            for (int j = 1; j<m ; j++) {
-
-            }
+        else if (n < 0) {
+            return 0;
         }
-        return memo[n][n];
+        else if (table[n] != 0){
+            return table[n];
+        }
+        int a = stepPerms(n-1, table);
+        int b = stepPerms(n-2, table);
+        int c = stepPerms(n-3, table);
+        table[n] = sm(sm(a, b), c);
+        return table[n];
+    }
+
+    private static int anotherWay(int n) {
+        int memo[] = new int [3];
+        memo[0] = 1;
+        memo[1] = 2;
+        memo[2] = 4;
+        if (n < 4)
+            return memo[n-1];
+        for (int i = 3; i<n; i++) {
+            int acc = 0;
+            acc = sm(memo[0], sm(memo[1], sm(memo[2], acc)));
+            memo[i%3] = acc;
+        }
+        return memo[(n-1)%3];
     }
 
     public static void main(String[] args) {
-        System.out.println(stepPerms(6));
-        int [] memo = new int[6];
+
+        for (int i = 7; i < 37 ; i++) {
+            //System.out.println(stepPerms(i));
+            int [] memo = new int[i+1];
+            memo[1] = 1;
+            System.out.println(stepPerms(i, memo));
+            System.out.println(anotherWay(i));
+            System.out.println("");
+        }
+
+
         /*
         System.out.println(stepPerms(2));
         System.out.println(anotherWay(2));
