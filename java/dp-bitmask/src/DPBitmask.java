9a36bef7cdad64b58ee118ad4d632027b3b102b5
chrislucas <christoffer.luccas@gmail.com>
2018-08-27 18:53:50 -0300

    estudos

---
+++ java/dp-bitmask/src/DPBitmask.java
@@ -0,0 +1,12 @@
+
+/**
+ * https://www.geeksforgeeks.org/bitmasking-and-dynamic-programming-set-1-count-ways-to-assign-unique-cap-to-every-person/
+ *
+ * */
+
+public class DPBitmask {
+    public static void main(String[] args) {
+
+    }
+
+}
