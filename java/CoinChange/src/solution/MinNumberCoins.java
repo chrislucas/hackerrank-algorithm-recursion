fdf01d2ebb428bff2eb9e80459991726d23f6ed9
chrislucas <christoffer.luccas@gmail.com>
2018-08-28 18:07:00 -0300

    adicionando uma classe para estudar uma variacao do problema coin change

---
+++ java/CoinChange/src/solution/MinNumberCoins.java
@@ -0,0 +1,13 @@
+package solution;
+
+/**
+ * https://www.geeksforgeeks.org/find-minimum-number-of-coins-that-make-a-change/
+ * 
+ * */
+
+public class MinNumberCoins {
+
+    public static void main(String[] args) {
+
+    }
+}
